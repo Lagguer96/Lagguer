fd03a7078ae9b7c06a4f2c5b4345406ae0ef2a85
Lagguer96 <medina17796@gmail.com>
2023-06-13 02:16:20 -0400

    Added_scripts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InformationComponent } from './information/information.component';
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
-// import { MaintenancePageComponent } from './maintenance-page/maintenance-page.component';
+//import { MaintenancePageComponent } from './maintenance-page/maintenance-page.component';
 
 const routes: Routes = [
   { path: 'information', component: InformationComponent },
